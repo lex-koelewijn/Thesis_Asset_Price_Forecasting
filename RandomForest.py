2a334ff49074641c4ea65c2d566eab04d46b8533
lex-koelewijn <lex.koelewijn@gmail.com>
2021-03-11 16:39:50 +0100

    Random forest for MEV and TA with fixed seed

--- RandomForest.py
+++ RandomForest.py
@@ -15,8 +15,9 @@
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 from tqdm.notebook import tqdm
 
 pd.set_option('display.max_columns', None)
@@ -67,5 +68,76 @@ ta = ta.set_index('Date')
 ta = ta.loc[(ta.index >= '1950-12-01')]
 
 # # Random Forest
+# In the code below we will train a random forest for each macro economic variable and technical indicator separately using a rolling window of the past 12 months for each variable/indicator. The recorded R2 score is based on in sample analysis, but the MAE, MSE and MSE are calculated using out of sample analysis. Thus the random forest is trained ussing rolling windows from 1950:12 to 1965:12 yielding 180-12=168 rolling windows. The model is then assessed in terms of prediction accuracy in MAE, MSE and RMSE using data from 1966:01 to 2019:12 yielding 648 rolling windows. 
+# ### Macro Economic Variables
+
+# +
+#Shift equity premiumms such that they correspond to the 1 month out of sample corresponding to each window. 
+y = ep.shift(periods=-12)[:ep.shape[0]-12].reset_index(drop=True)
+
+#Convert y to a series with only log equity premium or simple equity premium 
+y = y['Log equity premium'].astype('float64')
+
+# +
+# Create empty dictionary
+rollingWindowsMEV = dict()
+
+#Fill the dictionairy with the 2D array with rolling windows for each variable. 
+for variable in mev.columns:
+    rollingWindowsMEV[variable] = createRollingWindow1D(mev[variable], 12)
+
+# +
+df = pd.DataFrame(columns=['Variable', 'R2', 'MAE', 'MSE', 'RMSE'])
+
+for variable in mev.columns:
+    X_train, X_test, y_train, y_test = train_test_split(rollingWindowsMEV[variable], y, train_size=168, random_state=0, shuffle=False)
+    reg = RandomForestRegressor(random_state = 42).fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    df = df.append(pd.Series({'Variable' : variable, 
+                              'R2':  reg.score(X_train, y_train), 
+                              'MAE': metrics.mean_absolute_error(y_test, y_pred),
+                              'MSE': metrics.mean_squared_error(y_test, y_pred), 
+                              'RMSE': np.sqrt(metrics.mean_squared_error(y_test, y_pred))}), ignore_index=True)
+# -
+
+df
+
+# ### Technical Indiciators
+#
+
+# +
+#Shift equity premiumms such that they correspond to the 1 month out of sample corresponding to each window. 
+y = ep.shift(periods=-12)[:ep.shape[0]-12].reset_index(drop=True)
+
+#Convert y to a series with only log equity premium or simple equity premium 
+y = y['Log equity premium'].astype('float64')
+
+# +
+# Create empty dictionary
+rollingWindowsTA = dict()
+
+#Fill the dictionairy with the 2D array with rolling windows for each variable. 
+for variable in ta.columns:
+    rollingWindowsTA[variable] = createRollingWindow1D(ta[variable], 12)
+
+# +
+df = pd.DataFrame(columns=['Variable', 'R2', 'MAE', 'MSE', 'RMSE'])
+
+for variable in ta.columns:
+    X_train, X_test, y_train, y_test = train_test_split(rollingWindowsTA[variable], y, train_size=168, random_state=0, shuffle=False)
+    reg = RandomForestRegressor(random_state = 42).fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    df = df.append(pd.Series({'Variable' : variable, 
+                              'R2':  reg.score(X_train, y_train), 
+                              'MAE': metrics.mean_absolute_error(y_test, y_pred),
+                              'MSE': metrics.mean_squared_error(y_test, y_pred), 
+                              'RMSE': np.sqrt(metrics.mean_squared_error(y_test, y_pred))}), ignore_index=True)
+# -
+
+df
+
+
+
+
 
 
