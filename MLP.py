66000ae00262bcaab10b8c8e82e3e50ac98955d2
lex-koelewijn <lex.koelewijn@gmail.com>
2021-04-19 15:51:31 +0200

    Include Collab Init and analysis per variable

--- MLP.py
+++ MLP.py
@@ -12,13 +12,12 @@
 #     name: python3
 # ---
 
-# +
+import keras
+import tensorflow
 import pandas as pd
 import numpy as np
-import keras 
 from utils.utils import *
 from utils.clarkWestTest import clarkWestTest
-
 from keras.models import Sequential
 from keras.layers import Dense, Activation, BatchNormalization
 from tensorflow.keras.callbacks import EarlyStopping
@@ -26,12 +25,66 @@ from tensorflow.keras import regularizers
 from keras import optimizers 
 from keras import metrics 
 from tqdm.notebook import tqdm
+from numpy.random import seed
+
+# ## Google Collab Setup 
+# The following blocks of code should be used when running the code on Google Collab. (Meaning the local set up block should be commented) 
+
+# +
+import sys
+from google.colab import drive
+import tensorflow as tf
+from google.colab import files
+import os
+
+# Mount my google drive where the input files are stored. 
+drive.mount('/content/drive')
+sys.path.append('/content/drive/MyDrive/RUG/Master thesis finance/utils')
+
+# Load the GPU provided by google
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
+
+#Set desired verbosity of tensorflow
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+# Read the data from the drive location
+path_to_data = '/content/drive/MyDrive/RUG/Master thesis finance/data'
+# Read in the relevant data for the analysis.
+ep = readEquityPremiumData(path_to_data)
+mev = readMacroEconomicVariableData(path_to_data)
+ta = readTechnicalIndicatorData(path_to_data)
+
+
 # -
 
-# Read in the relevant data for the analysis.
+def downloadFiles(directory):
+    """
+    Function which downloads all files in the directory specified in the Collab environment.   
+    """
+    for filename in os.listdir(directory):
+    files.download(directory+filename)
+
+
+# ## Local Setup
+# The code block below should be used when running the repository on a local machine. (Meaning the Google collab block should be commented)
+
 ep = readEquityPremiumData()
 mev = readMacroEconomicVariableData()
 ta = readTechnicalIndicatorData()
+
+# ## Global Setup
+# From hereonout all code works both on a local machine or in Google collab. The required settings that are unisversal will be applied below. 
+
+#Set fixed seed for python, numpy and tensorflow
+seed(42)
+np.random.seed(42)
+try:
+    tensorflow.random.set_seed(42)
+except:
+    tensorflow.set_random_seed(42)
 
 
 # ## Functions
@@ -56,9 +109,9 @@ def analyzeResults(results, resultsRF, method, dataset):
     return resultsRF
 
 
-def createModel(hidden):
+def createModel(X, y, inputUnits, inputShape, hidden):
     model = Sequential()
-    model.add(Dense(14, input_shape=(14, ), 
+    model.add(Dense(inputUnits, input_shape=inputShape, 
                     activation='relu', 
                     activity_regularizer=regularizers.l1(0.01)))
     model.add(BatchNormalization())
@@ -72,11 +125,11 @@ def createModel(hidden):
 
     model.compile(optimizer = 'sgd', loss = 'mean_squared_error', metrics = ['mean_squared_error'])
     early_stopping = EarlyStopping(monitor='val_loss', patience = 5, min_delta=0.001, mode = 'min')
-    model.fit(X_mev, y_mev, epochs=100, batch_size=256, validation_split = 0.2, callbacks=[early_stopping], verbose=0)
+    model.fit(X, y, epochs=100, batch_size=128, validation_split = 0.2, callbacks=[early_stopping], verbose=0)
     return model
 
 
-def trainMLP(X_mev, y_mev, window_size, hidden):
+def trainMLP(X_mev, y_mev, window_size, hidden, inputUnits, inputShape):
     results = pd.DataFrame(columns=['Date_t', 'Date_t1', 'Actual', 'Pred', 'HA']) 
     
     for i in tqdm(range(0, X_mev.shape[0]-window_size)):
@@ -92,7 +145,7 @@ def trainMLP(X_mev, y_mev, window_size, hidden):
         
          # Define and train the model in keras once every year
         if(i % 12 == 0):
-            model = createModel(hidden)
+            model = createModel(X, y, inputUnits, inputShape, hidden)
 
         #Make a 1 month OOS prediction of the current time point t.
         y_pred = model.predict(X_t)
@@ -111,7 +164,7 @@ def trainMLP(X_mev, y_mev, window_size, hidden):
     return results
 
 
-def modelTrainingSequence(X, y, window_size, hidden, dataset):
+def modelTrainingSequence(X, y, window_size, hidden, dataset, inputUnits, inputShape):
     performanceResults = pd.DataFrame(columns=['Method', 'Dataset', 'R2', 'CW']) 
     
     # For each of the network specifications, try to find saved outputs. Otherwise train and evaluate model and save the outcomes. 
@@ -120,7 +173,7 @@ def modelTrainingSequence(X, y, window_size, hidden, dataset):
             results = pd.read_parquet('output/MLP_' + str(dataset) +'_' + str(hidden) +'.gzip')
         except:
             print('No saved results found, running model estimation.')
-            results = trainMLP(X, y, window_size = window_size, hidden = hidden)
+            results = trainMLP(X, y, window_size = window_size, hidden = hidden, inputUnits = inputUnits, inputShape = inputShape)
             results.to_parquet('output/MLP_' + str(dataset) +'_' + str(hidden) +'.gzip', compression='gzip')
         performanceResults = analyzeResults(results, performanceResults, method = 'MLP '+str(hidden), dataset = dataset)
     
@@ -129,6 +182,7 @@ def modelTrainingSequence(X, y, window_size, hidden, dataset):
 
 
 # ### MLP MEV
+# Run the MLP for all the macroeconomic at once as training input. 
 
 window_size = 180
 hidden_sizes = [32, 16, 8, 4, 2] 
@@ -137,13 +191,12 @@ check_existence_directory(['output'])
 y_mev = ep.shift(periods=-1)[:ep.shape[0]-1].reset_index(drop=True)['Log equity premium'].astype('float64')
 X_mev = mev.iloc[:mev.shape[0]-1]
 
-resultsMLP = modelTrainingSequence(normalizeData(X_mev), y_mev, window_size, hidden_sizes, dataset = 'MEV')
+resultsMEVAll = modelTrainingSequence(normalizeData(X_mev), normalizeData(y_mev), window_size, hidden_sizes, dataset = 'MEV', inputUnits = 14, inputShape = (14,))
 
-resultsMLP
-
-# ### resultsMLP
+resultsMEVAll
 
 # ### MLP TA
+# Run the MLP for all the macroeconomic at once as training input. 
 
 window_size = 180
 hidden_sizes = [32, 16, 8, 4, 2] 
@@ -154,27 +207,59 @@ check_existence_directory(['output'])
 y_ta = ep.shift(periods=-1)[:ep.shape[0]-1].reset_index(drop=True)['Log equity premium'].astype('float64')
 X_ta = ta.iloc[:ta.shape[0]-1]
 
-resultsTA = modelTrainingSequence(normalizeData(X_ta), y_ta, window_size, hidden_sizes, dataset = 'TA')
+resultsTAAll = modelTrainingSequence(normalizeData(X_ta), normalizeData(y_ta), window_size, hidden_sizes, dataset = 'TA', inputUnits = 14, inputShape = (14,))
+
+resultsTAAll
+
+
+# # Analysis per Variable
+# Up till now we have trained models using a vector of all variables at each time point. In the analysis below models will be trained for each variable seperately with the same set up as above. This allows us to observe the predictive power of variables indivdually given the current model architecucture.
+
+def runAnalysisPerVariable(X_raw, y_raw, hidden, window_size, dataset, inputUnits, inputShape):
+    # Initialize empty datafram to contain the results. 
+    resultsDF = pd.DataFrame(columns=['Method', 'Dataset', 'R2', 'CW']) 
+    
+    # Init y
+    y = y_raw.shift(periods=-1)[:y_raw.shape[0]-1].reset_index(drop=True)['Log equity premium'].astype('float64')
+
+    for variable in X_raw.columns:
+        # Select current variable and reshape such that pandas and numpy understand each other. 
+        X = X_raw.iloc[:X_raw.shape[0]-1][variable]
+        X = pd.DataFrame(X.values.reshape(-1, 1))
+        
+        for hidden in hidden_sizes:
+            # If model has been trained already we load input, otherwise train model. 
+            try: 
+                results = pd.read_parquet('output/MLP_' + str(dataset) +'_' + str(hidden) + '_' + str(variable) + '.gzip')
+            except:
+                print('No saved results found, running model estimation.')
+                results = trainMLP(normalizeData(X), normalizeData(y), window_size = window_size, hidden = hidden, inputUnits = inputUnits, inputShape = inputShape)
+                results.to_parquet('output/MLP_' + str(dataset) +'_' + str(hidden) + '_' + str(variable) + '.gzip', compression='gzip')
+                
+            #Analyze the results
+            resultsDF = analyzeResults(results, resultsDF, method = 'MLP '+str(hidden), dataset =   dataset + ': ' + str(variable))
+            
+    return resultsDF
+    
+
+
+# ### Macroeconomic variables
+
+resultsMEV = runAnalysisPerVariable(mev, ep, hidden_sizes,  window_size, dataset = 'MEV', inputUnits = 1, inputShape = (1,))
+
+resultsMEV
+
+# ### Technical Indicators
+
+resultsTA = runAnalysisPerVariable(ta, ep, hidden_sizes,  window_size, dataset = 'TA', inputUnits = 1, inputShape = (1,))
 
 resultsTA
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+with pd.ExcelWriter('output/MLP.xlsx') as writer:
+    resultsMEVAll.to_excel(writer, sheet_name='Accuracy MEV')
+    resultsTAAll.to_excel(writer, sheet_name='Accuracy TA')
+    resultsMEV.to_excel(writer, sheet_name='MEV Variables')
+    resultsTA.to_excel(writer, sheet_name='TA Variables')
 
 
 
